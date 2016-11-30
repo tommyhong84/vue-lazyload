52c38e689884a2ab6aa29d6b6c38265b09eee339
Myth <iammyth@iCloud.com>
2016-11-30 10:28:33 +0800

    Update vue-lazyload.js
    1. Removed unexpected listeners
    2. Add click to reload image

--- vue-lazyload.js
+++ vue-lazyload.js
@@ -1,225 +1,255 @@
-const Promise = require('es6-promise').Promise
+const inBrowser = typeof window !== 'undefined'
 
-export default (Vue, { preLoad = 1.3, error, loading, attempt = 3 }) => {
-    const isVueNext = Vue.version.split('.')[0] === '2'
-    const DEFAULT_URL = 'data:img/jpg;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABAQMAAAAl21bKAAAAA1BMVEXs7Oxc9QatAAAACklEQVQI12NgAAAAAgAB4iG8MwAAAABJRU5ErkJggg=='
+if (!Array.prototype.$remove) {
+  Object.defineProperty(Array.prototype, '$remove', {
+    value: function (item) {
+      if (!this.length) return
+      const index = this.indexOf(item)
+      if (index > -1) {
+        return this.splice(index, 1)
+      }
+    },
+    writable: false,
+    enumerable: false,
+    configurable: false
+  })
+}
 
-    const Init = {
-        preLoad: preLoad,
-        error: error || DEFAULT_URL,
-        loading: loading || DEFAULT_URL,
-        hasbind: false,
-        try: attempt
+export default (Vue, Options = {}) => {
+  const isVueNext = Vue.version.split('.')[0] === '2'
+  const DEFAULT_URL = 'data:image/gif;base64,R0lGODlhAQABAAAAACwAAAAAAQABAAA='
+  const ListenEvents = ['scroll', 'wheel', 'mousewheel', 'resize', 'animationend', 'transitionend']
+
+  const Init = {
+    preLoad: Options.preLoad || 1.3,
+    error: Options.error || DEFAULT_URL,
+    loading: Options.loading || DEFAULT_URL,
+    attempt: Options.attempt || 3,
+    scale: Options.scale || inBrowser ? window.devicePixelRatio : 1,
+    hasbind: false
+  }
+
+  const Listeners = []
+  const imageCache = []
+
+  const throttle = function (action, delay) {
+    let timeout = null
+    let lastRun = 0
+    return function () {
+      if (timeout) {
+        return
+      }
+      let elapsed = Date.now() - lastRun
+      let context = this
+      let args = arguments
+      let runCallback = function () {
+        lastRun = Date.now()
+        timeout = false
+        action.apply(context, args)
+      }
+      if (elapsed >= delay) {
+        runCallback()
+      }
+      else {
+        timeout = setTimeout(runCallback, delay)
+      }
     }
+  }
 
-    const Listeners = []
-    const Loaded = []
-
-    const throttle = function (action, delay) {
-        let timeout = null
-        let lastRun = 0
-        return function () {
-            if (timeout) {
-                return
-            }
-            let elapsed = (+new Date()) - lastRun
-            let context = this
-            let args = arguments
-            let runCallback = function () {
-                    lastRun = +new Date()
-                    timeout = false
-                    action.apply(context, args)
-                }
-                
-            if (elapsed >= delay) {
-                runCallback()
-            }
-            else {
-                timeout = setTimeout(runCallback, delay)
-            }
-        }
+  const _ = {
+    on (el, type, func) {
+      el.addEventListener(type, func)
+    },
+    off (el, type, func) {
+      el.removeEventListener(type, func)
     }
+  }
 
-    const _ = {
-        on (el, type, func) {
-            el.addEventListener(type, func)
-        },
-        off (el, type, func) {
-            el.removeEventListener(type, func)
-        }
+  const lazyLoadHandler = throttle(() => {
+    for (let key in Listeners) {
+      checkCanShow(Listeners[key])
     }
+  }, 300)
 
-    const lazyLoadHandler = throttle(() => {
-        for (let i = 0, len = Listeners.length; i < len; ++i) {
-            checkCanShow(Listeners[i])
-        }
-    }, 300)
-
-    const onListen = (el, start) => {
-        if (start) {
-            _.on(el, 'scroll', lazyLoadHandler)
-            _.on(el, 'wheel', lazyLoadHandler)
-            _.on(el, 'mousewheel', lazyLoadHandler)
-            _.on(el, 'resize', lazyLoadHandler)
-            _.on(el, 'animationend', lazyLoadHandler)
-            _.on(el, 'transitionend', lazyLoadHandler)
-        } else {
-            Init.hasbind = false
-            _.off(el, 'scroll', lazyLoadHandler)
-            _.off(el, 'wheel', lazyLoadHandler)
-            _.off(el, 'mousewheel', lazyLoadHandler)
-            _.off(el, 'resize', lazyLoadHandler)
-            _.off(el, 'animationend', lazyLoadHandler)
-            _.off(el, 'transitionend', lazyLoadHandler)
-        }
-    }
-
-    const checkCanShow = function(listener) {
-        if (Loaded.indexOf(listener.src) > -1) return setElRender(listener.el, listener.bindType, listener.src, 'loaded')
-        let rect = listener.el.getBoundingClientRect()
-        
-        if ((rect.top < window.innerHeight * Init.preLoad && rect.bottom > 0) && (rect.left < window.innerWidth * Init.preLoad && rect.right > 0)) {
-            render(listener)
-        }
-    }
-
-    const setElRender = (el, bindType, src, state) => {
-        if (!bindType) {
-            el.setAttribute('src', src)
-        } else {
-            el.setAttribute('style', bindType + ': url(' + src + ')')
-        }
-        el.setAttribute('lazy', state)
-    }
-
-    const render = function(item) {
-        if (item.try >= Init.try) return false
-
-        item.try++
-
-        loadImageAsync(item)
-            .then((url) => {
-                let index = Listeners.indexOf(item)
-                if (index !== -1) {
-                    Listeners.splice(index, 1)
-                }
-                setElRender(item.el, item.bindType, item.src, 'loaded')
-                Loaded.push(item.src)
-            })
-            .catch((error) => {
-                setElRender(item.el, item.bindType, item.error, 'error')
-                _.on(item.el, 'click', function(){
-                    _.off(item.el, 'click')
-                    loadImageAsync(item)
-                })
-            })
-    }
-
-    const loadImageAsync = (item) => {
-        return new Promise((resolve, reject) => {
-            let image = new Image()
-            image.src = item.src
-
-            image.onload = function () {
-                resolve(item.src)
-            }
-
-            image.onerror = function () {
-                reject()
-            }
-        })
-    }
-
-    const componentWillUnmount = (el, binding, vnode, OldVnode) => {
-        if (!el) return
-
-        for (let i = 0, len = Listeners.length; i < len; i++) {
-            if (Listeners[i] && Listeners[i].el === el) {
-                Listeners.splice(i, 1)
-            }
-        }
-
-        if (Init.hasbind && Listeners.length == 0) {
-            onListen(window, false)
-        }
-    }
-
-    const checkElExist = (el) => {
-        let hasIt = false
-
-        Listeners.forEach((item) => {
-            if (item.el === el) hasIt = true
-        })
-
-        if (hasIt) {
-            return Vue.nextTick(() => {
-                lazyLoadHandler()
-            })
-        }
-        return hasIt
-    }
-
-    const addListener = (el, binding, vnode) => {
-        if (el.getAttribute('lazy') === 'loaded') return
-        if (checkElExist(el)) return
-
-        let parentEl = null
-        let imageSrc = binding.value
-        let imageLoading = Init.loading
-        let imageError = Init.error
-
-        if (typeof(binding.value) !== 'string') {
-            imageSrc = binding.value.src
-            imageLoading = binding.value.loading || Init.loading
-            imageError = binding.value.error || Init.error
-        }
-        if (binding.modifiers) {
-            parentEl = window.document.getElementById(Object.keys(binding.modifiers)[0])
-        }
-
-        setElRender(el, binding.arg, imageLoading, 'loading')
-
-        Vue.nextTick(() => {
-            Listeners.push({
-                bindType: binding.arg,
-                try: 0,
-                parentEl: parentEl,
-                el: el,
-                error: imageError,
-                src: imageSrc
-            })
-            lazyLoadHandler()
-            if (Listeners.length > 0 && !Init.hasbind) {
-                Init.hasbind = true
-                onListen(window, true)
-            }
-            if (parentEl) {
-                onListen(parentEl, true)
-            }
-        })
-    }
-
-    if (isVueNext) {
-        Vue.directive('lazy', {
-            bind: addListener,
-            update: addListener,
-            componentUpdated: lazyLoadHandler,
-            unbind : componentWillUnmount
-        })
+  const onListen = (el, start) => {
+    if (start) {
+      ListenEvents.forEach((evt) => {
+        _.on(el, evt, lazyLoadHandler)
+      })
     } else {
-        Vue.directive('lazy', {
-            bind: lazyLoadHandler,
-            update (newValue, oldValue) {
-                addListener(this.el, {
-                    modifiers: this.modifiers,
-                    arg: this.arg,
-                    value: newValue,
-                    oldValue: oldValue
-                })
-            },
-            unbind () {
-                componentWillUnmount(this.el)
-            }
-        })
+      Init.hasbind = false
+      ListenEvents.forEach((evt) => {
+        _.off(el, evt, lazyLoadHandler)
+      })
     }
+  }
+
+  const checkCanShow = (listener) => {
+    if (imageCache.indexOf(listener.src) > -1) {
+      Listeners.$remove(listener)
+      return setElRender(listener.el, listener.bindType, listener.src, 'loaded')
+    }
+    let rect = listener.el.getBoundingClientRect()
+
+    if ((rect.top < window.innerHeight * Init.preLoad && rect.bottom > 0) && (rect.left < window.innerWidth * Init.preLoad && rect.right > 0)) {
+      render(listener)
+    }
+  }
+
+  const setElRender = (el, bindType, src, state) => {
+    if (!bindType) {
+      if (el.src !== src) {
+        el.setAttribute('src', '')
+      }
+      el.setAttribute('src', src)
+    } else {
+      el.setAttribute('style', bindType + ': url(' + src + ')')
+    }
+    el.setAttribute('lazy', state)
+  }
+
+
+  const render = (item) => {
+    if (item.attempt >= Init.attempt) return false
+
+    item.attempt++
+
+    const resolve = (image) => {
+      setElRender(item.el, item.bindType, item.src, 'loaded')
+      imageCache.push(item.src)
+      Listeners.$remove(item)
+    }
+
+    const reject = (error) => {
+      setElRender(item.el, item.bindType, item.error, 'error')
+      Listeners.$remove(item)
+      _.on(item.el, 'click', function (e) {
+        e.stopPropagation()
+        setElRender(item.el, item.bindType, Options.loading || DEFAULT_URL, 'loading')
+        _.off(item.el, 'click')
+        loadImageAsync(item, resolve, reject)
+      })
+    }
+
+    loadImageAsync(item, resolve, reject)
+  }
+
+  const loadImageAsync = (item, resolve, reject) => {
+
+    let image = new Image()
+    image.src = item.src
+
+    image.onload = function () {
+      resolve({
+        naturalHeight: image.naturalHeight,
+        naturalWidth: image.naturalWidth,
+        src: item.src
+      })
+    }
+
+    image.onerror = function (e) {
+      reject(e)
+    }
+  }
+
+  const componentWillUnmount = (el, binding, vnode, OldVnode) => {
+    if (!el) return
+
+    for (let i = 0, len = Listeners.length; i < len; i++) {
+      if (Listeners[i] && Listeners[i].el === el) {
+        Listeners.splice(i, 1)
+      }
+    }
+
+    if (Init.hasbind && Listeners.length === 0) {
+      onListen(window, false)
+    }
+  }
+
+  const checkElExist = (el) => {
+    let hasIt = false
+
+    Listeners.forEach((item) => {
+      if (item.el === el) hasIt = true
+    })
+
+    if (hasIt) {
+      return Vue.nextTick(() => {
+        lazyLoadHandler()
+      })
+    }
+    return hasIt
+  }
+
+  const addListener = (el, binding, vnode) => {
+    if (~(['error', 'loaded'].indexOf(el.getAttribute('lazy'))) && binding.oldValue === binding.value) {
+      return
+    }
+    if (checkElExist(el)) return
+
+    let parentEl = null
+    let imageSrc = binding.value
+    let imageLoading = Init.loading
+    let imageError = Init.error
+
+    if (typeof (binding.value) !== 'string' && binding.value) {
+      imageSrc = binding.value.src
+      imageLoading = binding.value.loading || Init.loading
+      imageError = binding.value.error || Init.error
+    }
+
+    if (imageCache.indexOf(imageSrc) > -1) return setElRender(el, binding.arg, imageSrc, 'loaded')
+
+    setElRender(el, binding.arg, imageLoading, 'loading')
+
+    Vue.nextTick(() => {
+      if (binding.modifiers) {
+        parentEl = window.document.getElementById(Object.keys(binding.modifiers)[0])
+      }
+
+      Listeners.push({
+        bindType: binding.arg,
+        attempt: 0,
+        parentEl: parentEl,
+        el: el,
+        error: imageError,
+        src: imageSrc
+      })
+      lazyLoadHandler()
+
+      if (Listeners.length > 0 && !Init.hasbind) {
+        Init.hasbind = true
+        onListen(window, true)
+
+        if (parentEl) {
+          onListen(parentEl, true)
+        }
+      }
+    })
+  }
+
+  if (isVueNext) {
+    Vue.directive('lazy', {
+      bind: addListener,
+      update: addListener,
+      inserted: addListener,
+      componentUpdated: lazyLoadHandler,
+      unbind: componentWillUnmount
+    })
+  } else {
+    Vue.directive('lazy', {
+      bind: lazyLoadHandler,
+      update (newValue, oldValue) {
+        addListener(this.el, {
+          modifiers: this.modifiers,
+          arg: this.arg,
+          value: newValue,
+          oldValue: oldValue
+        })
+      },
+      unbind () {
+        componentWillUnmount(this.el)
+      }
+    })
+  }
 }
